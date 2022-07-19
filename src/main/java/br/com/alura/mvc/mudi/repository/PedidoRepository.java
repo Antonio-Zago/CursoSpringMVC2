d0efed46068b818b64ae0bbd83d8de5f78924b37
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2022-07-19 19:58:30 -0300

    Cache e paginação

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.repository;
 
+
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +15,9 @@ import br.com.alura.mvc.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-	List<Pedido> findByStatus(StatusPedido aguardando);
+	
+	@Cacheable("books")
+	List<Pedido> findByStatus(StatusPedido aguardando, Pageable pageable);
 	
 	@Query("select p from Pedido p join p.user u where u.username = :username")
 	List<Pedido> findAllByUsuario(@Param("username")String username);
